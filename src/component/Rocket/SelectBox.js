039ee908948b205a44cf80ff52f8ec665ea6185a
RyanWijalaksana <ryan.wijalaksana@tokopedia.com>
2019-05-14 11:55:54 +0700

    hooks01

--- src/component/Rocket/SelectBox.js
+++ src/component/Rocket/SelectBox.js
@@ -1,63 +1,63 @@
 import React, { Component, Fragment } from "react";
 
 class SelectBox extends Component {
+    state = {
+        rockets: [],
+        loading: false
+    };
 
-	state = {
-		rockets: [],
-		loading: false
-	};
+    componentDidMount() {
+        this.setState({ loading: true });
 
-	componentDidMount() {
-		this.setState({ loading: true });
+        fetch("https://api.spacexdata.com/v3/rockets")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch");
+                }
+                return response.json();
+            })
+            .then(data => {
+                //console.log("data", data);
+                this.setState({
+                    rockets: data,
+                    loading: false
+                });
+            })
+            .catch(error => console.log("error : ", error));
+    }
 
-		fetch("https://api.spacexdata.com/v3/rockets")
-			.then(response => {
-				if (!response.ok) {
-					throw new Error("Failed to fetch");
-				}
-				return response.json();
-			})
-			.then(data => {
-				//console.log("data", data);
-				this.setState({
-					rockets: data,
-					loading: false
-				});
-			})
-			.catch(error => console.log("error : ", error));
-	}
+    handleChange = e => {
+        const { onSelectBoxChange } = this.props;
 
-	handleChange = e => {
-		const { onSelectBoxChange } = this.props;
+        if (onSelectBoxChange) {
+            onSelectBoxChange(e.target.value);
+        }
+    };
 
-		if(onSelectBoxChange){
-			onSelectBoxChange(e.target.value);
-		}
-	}
+    render() {
+        const { rockets, loading } = this.state;
 
-	render() {
-		const { rockets, loading } = this.state;
-
-		return (
-			<Fragment>
-				{loading ? (
-					<div>Loading..</div>
-				) : (
-					<select 
-						name="" 
-						id=""
-						onChange={e => this.handleChange(e)}
-					>
-					{rockets.map((rocket, index) => (
-						<option key={rocket.id} value={rocket.rocket_id}>
-							{rocket.rocket_name}
-						</option>
-					))}
-					</select>
-				)}
-			</Fragment>
-		);
-	}
+        return (
+            <Fragment>
+                {loading ? (
+                    <div>Loading..</div>
+                ) : (
+                    <select name="" id="" onChange={e => this.handleChange(e)}>
+                        {rockets &&
+                            rockets.length > 0 &&
+                            rockets.map((rocket, index) => (
+                                <option
+                                    key={rocket.id}
+                                    value={rocket.rocket_id}
+                                >
+                                    {rocket.rocket_name}
+                                </option>
+                            ))}
+                    </select>
+                )}
+            </Fragment>
+        );
+    }
 }
 
 export default SelectBox;
